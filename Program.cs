70dc98ab9eed85349a6f89a1f565b53c717e3731
Martina Gajduskova <gajduskova.marta@gmail.com>
2022-09-09 21:33:16 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace prvniprogram
+    {
+        class Animal
+        {
+            string name;
+            int legs;
+            string sound;
+            public double weight;
+            bool mammal;
+
+            public bool IsHeavy()
+            {
+                if (this.weight > 30.0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            void MakeSound()
+            {
+                System.Console.WriteLine(this.sound);
+            }
+
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                System.Console.WriteLine("salut");
+                Animal zvire1 = new Animal();
+                zvire1.weight = 50;
+                System.Console.WriteLine(zvire1.IsHeavy());
+            }
+        }
+    }
